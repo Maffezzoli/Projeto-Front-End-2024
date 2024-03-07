7ce897ecfde50bdce7da5444de7c2fffd7b9189f
Daniel Maffezzoli <Maffezzolidaniel78@gmail.com>
2024-03-07 11:05:00 -0300

    markdown

---
+++ Entendimento_Inicial.mk
@@ -0,0 +1,10 @@
+Projeto DosImagem
+
+Problema:
+	Interface muito complicada
+Objetivo:
+	Facilitar o uso da interface, por meio de um “fluxo”
+-Fluxo:
+	Instaurar um passo a passo para o “tratamento” da imagem, de forma que fique intuitivo para usuários não treinados.
+-Dúvidas:
+	*Integração com o Back-End
